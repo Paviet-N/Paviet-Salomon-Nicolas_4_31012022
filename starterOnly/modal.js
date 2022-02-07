572143f758c7bd3be594a6e9862d8ca9a8d1c28e
Nicolas Paviet <Nicolas@LeNewt.com>
2022-02-07 14:25:21 +0100

    All Issue completed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,27 +1,40 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelector(".form-close");
 const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelectorAll(".close");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalClose.forEach((btnclose) => btnclose.addEventListener("click" , closeModal));
+
+//
+//  Functions
+//
+
+// responsive nav
+function editNav() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-function closeModal(){
+// close modal form
+function closeModal() {
   modalbg.style.display = "none";
 }
+
+
+//
+//  Events
+//
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+modalClose.addEventListener("click", closeModal);
